90750d8120f6112be081acbb995eec3703f72ce9
resetss <imstephen.1sw@gmail.com>
2025-01-11 06:51:12 -0500

    Corrected export path and added message to consol.

--- Tiles/src/Tiles/TileEditor.cpp
+++ Tiles/src/Tiles/TileEditor.cpp
@@ -543,6 +543,8 @@ void TileEditor::RenderExport()
 
     if (ImGui::Button("Export Image"))
     {
+        m_ConsolOutputs.push_back("Exporting to " + m_ExportPath); 
+
         TileObject tileObject; 
 
         Lumina::ShaderProgram shader;
@@ -588,8 +590,7 @@ void TileEditor::RenderExport()
 
         m_Atlas.Unbind();
         renderer.End();
-        std::string path = "res/output.png";
-        renderer.SaveFrameBufferToImage(path);
+        renderer.SaveFrameBufferToImage(m_ExportPath);
     }
 
     ImGui::End();
