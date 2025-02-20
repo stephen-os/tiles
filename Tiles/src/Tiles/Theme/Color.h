dd939eed78aab5080351325a0b84d3a2902aabe2
Resetss <imstephen.1sw@gmail.com>
2025-02-20 15:41:26 -0500

    Added comment to color.

--- Tiles/src/Tiles/Theme/Color.h
+++ Tiles/src/Tiles/Theme/Color.h
@@ -8,7 +8,7 @@ namespace Tiles
 	{
         static constexpr ImU32 FILL_COLOR = IM_COL32(255, 255, 255, 255);               // White
         static constexpr ImU32 BACKGROUND_COLOR = IM_COL32(77, 77, 77, 255);            // Dark Gray
-        static constexpr ImU32 OUTLINE_COLOR = IM_COL32(169, 169, 169, 255);            
+        static constexpr ImU32 OUTLINE_COLOR = IM_COL32(169, 169, 169, 255);            // Lighter Gray
         static constexpr ImU32 SELECTION_BORDER_COLOR = IM_COL32(255, 165, 0, 255);     // Orange
         static constexpr ImU32 DEAULT_BORDER_COLOR = IM_COL32(51, 51, 51, 255);         // Black
 
