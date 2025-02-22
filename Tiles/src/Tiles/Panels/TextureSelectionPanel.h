08073d7ea59133a276636c1c2d96c0ab9f4d5e00
Resetss <imstephen.1sw@gmail.com>
2025-02-21 20:05:11 -0500

    Some members were coppied, not referenced.

--- Tiles/src/Tiles/Panels/TextureSelectionPanel.h
+++ Tiles/src/Tiles/Panels/TextureSelectionPanel.h
@@ -21,8 +21,8 @@ namespace Tiles
         void OnUIRender();
 
         // Setters
-        void SetTextureAtlas(Shared<Atlas> atlas) { m_Atlas = atlas; }
-        void SetTextureSelection(Shared<TextureSelection> textureSelection) { m_TextureSelection = textureSelection; }
+        void SetTextureAtlas(Shared<Atlas>* atlas) { m_Atlas = atlas; }
+        void SetTextureSelection(Shared<TextureSelection>& textureSelection) { m_TextureSelection = textureSelection; }
         void SetToolSelection(Shared<ToolSelection>& toolSelection) { m_ToolSelection = toolSelection; }
     private:
         // UI Rendering Methods
