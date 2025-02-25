0c8eb80d351ec2db83ee17f2362820ff40d80652
Resetss <imstephen.1sw@gmail.com>
2025-02-25 13:43:41 -0500

    Moved export shader code to file ExportShader.

--- Tiles/src/Tiles/Core/Exporter.cpp
+++ Tiles/src/Tiles/Core/Exporter.cpp
@@ -5,6 +5,8 @@
 #include "Lumina/Renderer/Renderer.h"
 #include "Lumina/Renderer/Shapes/Quad.h"
 
+#include "Lumina/Utils/FileReader.h"
+
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "Tile.h"
@@ -17,59 +19,9 @@ namespace Tiles
     {
         Lumina::Quad quad;
 
-        const std::string vertexShader = R"(
-        #version 330 core
-
-        // Per Vertex
-        layout(location = 0) in vec3 a_Position;
-        layout(location = 1) in vec3 a_Normal;
-        layout(location = 2) in vec2 a_TexCoord;
-
-        uniform mat4 u_Transform;  // Per-tile transformation
-        uniform mat4 u_OrthoProjection;  // Covers the entire tile grid
-        uniform float u_NumberOfRows; 
-        uniform vec2 u_Offset; 
-
-        out vec3 v_Normal;
-        out vec2 v_TexCoord;
-        out vec3 v_Position;
-
-        void main()
-        {
-            // Apply only the per-tile transform and the orthographic projection
-            gl_Position = u_OrthoProjection * u_Transform * vec4(a_Position, 1.0);
-            v_Normal = mat3(transpose(inverse(u_Transform))) * a_Normal;
-            v_Position = vec3(u_Transform * vec4(a_Position, 1.0));
-            v_TexCoord = (a_TexCoord / u_NumberOfRows) + u_Offset;
-        }
-    )";
-
-        const std::string fragmetShader = R"(
-        #version 330 core
-
-        in vec3 v_Normal;
-        in vec3 v_Position;
-        in vec2 v_TexCoord;
-
-        out vec4 FragColor;
-
-        uniform vec3 lightColor = vec3(1.0, 1.0, 1.0);
-        uniform sampler2D u_Texture;
-
-        void main()
-        {
-            // Uniform lighting (simulate ambient lighting)
-            vec3 uniformLighting = lightColor;
-
-            // Texture color
-            vec4 texColor = texture(u_Texture, v_TexCoord);
-            if (texColor.a < 0.1)
-                discard;
-
-            FragColor = texColor;
-        }
-    )";
-
+        const std::string vertexShader = Lumina::ReadFile("res/shaders/ExportShader.vert");
+        const std::string fragmetShader = Lumina::ReadFile("res/shaders/ExportShader.frag");
+       
         Lumina::ShaderProgram shader;
         shader.SetSource(vertexShader, fragmetShader);
 
--- Tiles/src/Tiles/Core/Exporter.h
+++ Tiles/src/Tiles/Core/Exporter.h
@@ -2,7 +2,6 @@
 
 #include "Layers.h"
 #include "Atlas.h"
-
 #include "Base.h"
 
 namespace Tiles
