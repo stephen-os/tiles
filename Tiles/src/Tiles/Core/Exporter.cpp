cc5a1966304e0563db533b4a1d79952171ddaf60
stephen-os <ImStephen.1sw@gmail.com>
2025-04-14 05:29:48 -0400

    Updated exporter to use Lumina batch renderer.

--- Tiles/src/Tiles/Core/Exporter.cpp
+++ Tiles/src/Tiles/Core/Exporter.cpp
@@ -1,10 +1,11 @@
 #include "Exporter.h"
 
+#include "Lumina/Core/Log.h"
+
+#include "Lumina/Renderer/Cameras/OrthographicCamera.h"
 #include "Lumina/Renderer/ShaderProgram.h"
 #include "Lumina/Renderer/FrameBuffer.h"
 #include "Lumina/Renderer/Renderer.h"
-
-#include "Lumina/Utils/FileReader.h"
 
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -16,19 +17,20 @@ namespace Tiles
 
     void Exporter::Export(Shared<Layers>& layers, Shared<Lumina::TextureAtlas>& atlas, std::string& filepath, std::string& filename, std::vector<size_t>& groupings)
     {
-#if 0
-        Lumina::Quad quad;
-
-        const std::string vertexShader = Lumina::ReadFile("res/shaders/ExportShader.vert");
-        const std::string fragmetShader = Lumina::ReadFile("res/shaders/ExportShader.frag");
-       
-        Lumina::ShaderProgram shader;
-        shader.SetSource(vertexShader, fragmetShader);
+        if (!atlas->HasTexture())
+        {
+            LUMINA_LOG_WARN("No atlas has been created.");
+            return;
+        }
 
         int outputWidth = static_cast<int>(layers->GetWidth() * m_Resolution);
         int outputHeight = static_cast<int>(layers->GetHeight() * m_Resolution);
 
-        glm::mat4 orthoProjection = glm::ortho(0.0f, float(layers->GetWidth()), 0.0f, float(layers->GetHeight()), -1.0f, 2.0f);
+        Lumina::OrthographicCamera camera(0.0f, layers->GetWidth(), 0.0f, layers->GetHeight(), -1.0f, 2.0f);
+
+        Lumina::QuadAttributes attributes;
+        attributes.Size = { 0.5f, 0.5f };
+        attributes.Texture = atlas->GetTexture(); 
 
         size_t offset = 0;
         size_t index = 1; 
@@ -37,16 +39,9 @@ namespace Tiles
             if (group == 0)
                 continue;
 
-            Lumina::Renderer::Begin();
-            Lumina::Renderer::OnWindowResize(outputWidth, outputHeight);
-            Lumina::Renderer::Clear();
-
-            atlas->Bind();
-
-            shader.Bind();
-            shader.SetUniformMatrix4fv("u_OrthoProjection", orthoProjection);
-            shader.SetUniform1f("u_NumberOfRows", static_cast<float>(atlas->GetWidth()));
-
+            Lumina::Renderer::Begin(camera);
+            Lumina::Renderer::SetResolution(outputWidth, outputHeight);
+            
             for (size_t l = offset; l < (offset + group) && l < layers->GetSize(); l++)
             {
                 Layer& layer = layers->GetLayer(l);
@@ -57,24 +52,21 @@ namespace Tiles
                         Tile& tile = layer.GetTile(y, x);
                         if (tile.GetTextureIndex() != -1)
                         {
-                            glm::mat4 transform = glm::translate(glm::mat4(1.0f), glm::vec3(x, y, l * 0.01f));
-                            glm::vec2 offset = atlas->GetOffset(static_cast<int>(tile.GetTextureIndex()));
+                            attributes.Position = { x + 0.5f, y + 0.5f, 0.0f };
+                            attributes.TextureCoords = atlas->GetTextureCoords(tile.GetTextureIndex());
 
-                            shader.SetUniformMatrix4fv("u_Transform", transform);
-                            shader.SetUniform2fv("u_Offset", offset);
-                            Lumina::Renderer::Draw(quad.GetVertexArray());
+                            Lumina::Renderer::DrawQuad(attributes);
                         }
                     }
                 }
             }
 
             Lumina::Renderer::End();
-            Lumina::Renderer::SaveFrameBufferToImage(filepath + "/" + filename + "-" + std::to_string(index) + ".png");
+            Lumina::Renderer::SaveFrameBufferToFile(filepath + "/" + filename + "-" + std::to_string(index) + ".png");
 
             offset += group;
             index++;
 		}
-#endif
     }
 
 }
